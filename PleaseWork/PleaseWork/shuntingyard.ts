63edca477c938661c81405434e23c51fe16a8faa
dragonsarebest <joshuabyron61@yahoo.com>
2020-02-24 19:09:23 -0500

    whoops frogot to push Jimbo

--- PleaseWork/PleaseWork/shuntingyard.ts
+++ PleaseWork/PleaseWork/shuntingyard.ts
@@ -19,132 +19,149 @@ class TreeNode {
 }
 
 
-export class parse {
-    operatorStack: Array<TreeNode>;
-    operandStack: Array<TreeNode>;
-    fs = require("fs");
-
-    associativity =
-        {
-            "LP": "left",
-            "CMA": "right",
-            "ADDOP": "left",
-            "MULOP": "left",
-            "NEGATE": "right",
-            "POWOP": "right",
-            "FUNCCALL": "left"
-        }
-
-    operators =
-        {
-            "LP": 1,
-            "CMA": 2,
-            "ADDOP": 3,
-            "NEGATE": 4,
-            "POWOP": 5,
-            "FUNCCALL": 6
-        }
-    //higher number means higher priority
-
-    arity =
-        {
-            "LP": 2,
-            "CMA": 2,
-            "ADDOP": 2,
-            "MULOP": 2,
-            "NEGATE": 1,
-            "POWOP": 2,
-            "FUNCCALL": 2
-        }
-    //all unary opperations become 2 in arity
-
-    doOperation() {
-        let opNode = this.operatorStack.pop();
-        let c1 = this.operandStack.pop();
-        if (this.arity[opNode.sym] == 2) {
-            let c2 = this.operandStack.pop();
-            opNode.addChild(c2);
-        }
-        opNode.addChild(c1);
-        this.operandStack.push(opNode);
+function doOperation(operatorStack: Array<TreeNode>, operandStack: Array<TreeNode>, arity: Map<string, number>) {
+    let opNode = operatorStack.pop();
+    console.log("Opperation node: " + opNode.sym);
+    let c1 = operandStack.pop();
+    if (arity.get(opNode.sym) == 2) {
+        let c2 = operandStack.pop();
+        opNode.addChild(c2);
+        console.log("Adding to opperation's children: " + c2.sym);
     }
-
-    constructor(input: string) {
-
-        console.log(input);
-
-        let data: string = this.fs.readFileSync("myGrammar.txt", "utf8");
-        let gg = new Grammar(data);
-        let tokenizer = new Tokenizer(gg);
-        tokenizer.setInput(input);
-
-        this.operatorStack = Array<TreeNode>();
-        this.operandStack = Array<TreeNode>();
-
-        do {
-            let t = tokenizer.next();
-            let pt = new Token("nothing", "nothing", -1);
-            if (t.sym == "$")
-                break;
-            if (t.lexeme == "-") {
-                pt = tokenizer.previous();
-            }
-            if (pt.sym != "nothing" && (pt == undefined || pt.sym == "LPAREN" || pt.sym in this.operators)) {
-                t.sym = "NEGATE";
-            }
-            let sym = t.sym;
-            if (sym == "NUM" || sym == "ID") {
-                this.operandStack.push(new TreeNode(t.sym, t));
-            }
-            else {
-                console.log("not a num or id or negate");
-                if (this.associativity[sym] == "left") {
-                    while (true) {
-                        if (this.operatorStack.length == 0)
-                            break;
-                        let A = this.operatorStack[this.operatorStack.length - 1].sym;
-                        if (this.operators[A] < this.operators[sym]) {
-                            break;
-                        }
-                        this.doOperation();
-                    }
-                    this.operatorStack.push(new TreeNode(t.sym, t));
-                }
-                else {
-                    while (true) {
-                        if (this.operatorStack.length == 0)
-                            break;
-                        let A = this.operatorStack[this.operatorStack.length - 1].sym;
-                        if (this.operators[A] >= this.operators[sym]) {
-                            this.doOperation();
-                        }
-                        else {
-                            break;
-                        }
-                        this.doOperation();
-                    }
-                    this.operatorStack.push(new TreeNode(t.sym, t));
-
-                    while (this.operatorStack.length > 0) {
-                        this.doOperation();
-                    }
-                }
-            }
-
-        } while (true);
-
-        while (this.operatorStack.length > 0) {
-            this.doOperation();
-        }
-
-        console.log(this.operandStack[0]);
-        this.operandStack[0].children.forEach(element => {
-            console.log(element);
-        });
-
-    }
-
-
-
+    opNode.addChild(c1);
+    console.log("Adding to opperation's children: " + c1.sym);
+    operandStack.push(opNode);
 }
 
+export function parse(input: string) {
+    let operatorStack: Array<TreeNode>;
+    let operandStack: Array<TreeNode>;
+    let fs = require("fs");
+
+    let associativity: Map<string, string> = new Map<string, string>();
+    associativity.set("LP", "left");
+    associativity.set("CMA", "right");
+    associativity.set("MULOP", "left");
+    associativity.set("ADDOP", "left");
+    associativity.set("NEGATE", "right");
+    associativity.set("BITNOT", "right");
+    associativity.set("POWOP", "right");
+    associativity.set("FUNCCALL", "left");
+
+    let operators: Map<string, number> = new Map<string, number>();
+    operators.set("LP", 1);
+    operators.set("CMA", 2);
+    operators.set("ADDOP", 3);
+    operators.set("MULOP", 4);
+    operators.set("BITNOT", 5);
+    operators.set("POWOP", 6);
+    operators.set("FUNCCALL", 7);
+    //higher number means higher priority
+
+    let arity: Map<string, number> = new Map<string, number>();
+    arity.set("LP", 2);
+    arity.set("CMA", 2);
+    arity.set("ADDOP", 2);
+    arity.set("MULOP", 2);
+    arity.set("NEGATE", 1);
+    arity.set("POWOP", 2);
+    arity.set("FUNCCALL", 2);
+    arity.set("BITNOT", 1);
+    //all unary opperations become 1 in arity
+
+    console.log("INPUT:");
+    console.log(input);
+
+    let data: string = fs.readFileSync("myGrammar.txt", "utf8");
+    let gg = new Grammar(data);
+    let tokenizer = new Tokenizer(gg);
+    tokenizer.setInput(input);
+
+    operatorStack = Array<TreeNode>();
+    operandStack = Array<TreeNode>();
+
+    do {
+        let t = tokenizer.next();
+        let pt = new Token("nothing", "nothing", -1);
+        if (t.sym == "$")
+            break;
+        if (t.lexeme == "-") {
+            pt = tokenizer.previous;
+        }
+        if (pt == undefined || pt.sym == "LPAREN" || pt.sym in operators) {
+            t.sym = "NEGATE";
+        }
+        let sym = t.sym;
+
+        console.log("Token: " + t);
+
+        if (sym == "RP") {
+            while (true) {
+                if (operatorStack[operatorStack.length - 1].sym == "LP") {
+                    operatorStack.pop();
+                    break;
+                }
+                doOperation(operatorStack, operandStack, arity);
+            }
+            continue;
+        }
+        if (sym == "LP" || sym == "POWOP" || sym == "BITNOT" || sym == "NEGATE") {
+            operatorStack.push(new TreeNode(t.sym, t));
+            continue;
+        }
+        //THIS DOESN'T WORK AS INTENDED???
+
+        if (sym == "NUM" || sym == "ID") {
+            operandStack.push(new TreeNode(t.sym, t));
+        }
+        else {
+            console.log("not a num or id or negate");
+            if (associativity.get(sym) == "left") {
+                while (true) {
+                    if (operatorStack.length == 0)
+                        break;
+                    let A = operatorStack[operatorStack.length - 1].sym;
+                    if (operators.get(A) < operators.get(sym)) {
+                        break;
+                    }
+                    doOperation(operatorStack, operandStack, arity);
+                }
+                operatorStack.push(new TreeNode(t.sym, t));
+            }
+            else {
+                while (true) {
+                    if (operatorStack.length == 0)
+                        break;
+                    let A = operatorStack[operatorStack.length - 1].sym;
+                    if (operators.get(A) >= operators.get(sym)) {
+                        doOperation(operatorStack, operandStack, arity);
+                    }
+                    else {
+                        break;
+                    }
+                    doOperation(operatorStack, operandStack, arity);
+                }
+                operatorStack.push(new TreeNode(t.sym, t));
+
+                while (operatorStack.length > 0) {
+                    doOperation(operatorStack, operandStack, arity);
+                }
+            }
+        }
+
+    } while (true);
+
+    while (operatorStack.length > 0) {
+        doOperation(operatorStack, operandStack, arity);
+    }
+
+
+    let output = operandStack[0];
+    //console.log("OUTPUT:" + (output instanceof TreeNode));
+    //console.log(output);
+    //operandStack[0].children.forEach(element => {
+    //    console.log(element);
+    //});
+
+    return output;
+}
