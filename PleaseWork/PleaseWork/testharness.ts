7f9cc419045a53c30945989fb885f8f75a512d15
dragonsarebest <joshuabyron61@yahoo.com>
2020-04-07 14:35:34 -0400

    starting

--- PleaseWork/PleaseWork/testharness.ts
+++ PleaseWork/PleaseWork/testharness.ts
@@ -2,51 +2,35 @@
 
 declare var require:any;
 let fs = require("fs");
-import {makeTable} from "./LR";
+import {parse} from "./parser";
 
 class Test{
     name: string;
     grammar: string;
-    expectedReturn: number;
-    expectedTable: any;
+    input: string;
+    expectedTree: any;
 }
 
 function main(){
     
     let tests : Test[] = JSON.parse( fs.readFileSync("tests.txt", "utf8" ) );
-    tests.forEach( (t:Test) => {
+    tests.every( (t:Test) => {
         console.log(t.name);
-        let tpl : [Map<string,any>[], number];
-        tpl = makeTable( t.grammar );
-        let txt = outputTable(tpl[0]);
-        fs.writeFileSync( t.name+".table.txt", txt );
-        if( tpl[1] !== t.expectedReturn ) {
-            console.log("Expected return of "+t.expectedReturn+"but got",tpl[1]);
-            return;
+        try{
+            let root = parse( t.grammar, t.input );
+            if( t.expectedTree === undefined ){
+                console.log("Did not signal error on "+t.name);
+                return false;
+            }
+            fs.writeFileSync( t.name+".tree.dot", root );
+        } catch( e ){
+            if( t.expectedTree !== undefined ){
+                console.log("Signaled error on "+t.name);
+                return false;
+            }
         }
+        return true;
     });
-}
-
-function outputTable(T: Map<string,any>[] ){
-    let L:string[] = [];
-    T.forEach( ( row : Map<string,any>, idx: number ) => {
-        L.push("Table row "+idx);
-        let keys : string[] = [];
-        for(let key of row.keys() ){
-            keys.push(key);
-        }
-        keys.sort();
-        keys.forEach( (key: string) => {
-            let A: any = row.get(key);
-            let tmp: string;
-            if( A.action === "s" )
-                tmp = "shift and go to "+A.num;
-            else
-                tmp = "pop "+A.num+" and reduce to "+A.lhs;
-            L.push("    On "+key+", "+tmp);
-        });
-    });
-    return L.join("\n");
 }
 
 
